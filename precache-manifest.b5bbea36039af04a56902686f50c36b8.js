6bdc7bf642c2ce717bb99d22000f6b68a6a684f2
george-lk <>
2019-12-09 21:33:08 +0800

    Updates

---
+++ precache-manifest.b5bbea36039af04a56902686f50c36b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e24e29c940a639b93c5f69016c53d3",
+    "url": "/portfolio-front-page/index.html"
+  },
+  {
+    "revision": "7a4419f903272ea820b5",
+    "url": "/portfolio-front-page/static/css/main.966080a7.chunk.css"
+  },
+  {
+    "revision": "4362a4f312606a7c4f0e",
+    "url": "/portfolio-front-page/static/js/2.d85fda0a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio-front-page/static/js/2.d85fda0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a4419f903272ea820b5",
+    "url": "/portfolio-front-page/static/js/main.21dc3383.chunk.js"
+  },
+  {
+    "revision": "aa43374c79b622f3f23b",
+    "url": "/portfolio-front-page/static/js/runtime-main.da9179b5.js"
+  }
+]);
