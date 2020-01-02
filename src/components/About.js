33dfc5c0084bb0d28ece7e993b0123c8663dc2c8
george-lk <>
2020-01-02 09:02:18 +0800

    Add brief explanation in the about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,25 +4,68 @@ import React from 'react';
 function About(){
     return(
         <div className='about'>
-            <div className="header">ABOUT</div>
-            <div>I am an experience software engineer with 6 years of working experience.</div>
-            <div>I have experience in the following programming language</div>
-            <ul>
-                <li>Javascript</li>
-                <li>Python</li>
-                <li>C/C++</li>
-                <li>C#</li>
-                <li>Java</li>
-                <li>SQL</li>
-            </ul>
-            <div>I have experience in the following frameworks</div>
-            <ul>
-                <li>Nodejs</li>
-                <li>React</li>
-                <li>Expressjs</li>
-                <li>MySql</li>
-                <li>Unity Engine</li>
-            </ul>
+            <svg width='100vw' height='100vh' viewBox='0 0 2500 2000' >
+
+                <g className='textIntro'>
+                    <text   
+                            x='200px' y='100px'
+                            textAnchor='right'
+                            fontSize='140px'
+                            fill='hsl(170,100%,30%)'>
+                        2    ABOUT
+                    </text>
+                </g>
+
+                <g className='about_text1'>
+                    <text   x='400px' y='300px'
+                            textAnchor='left'
+                            fontSize='50px'
+                            fill='hsl(0,0%,50%)'>
+                                <tspan x='400' dy='10'>
+                                    I am an experience software developer with over 6 years of working experience.
+                                </tspan>
+                                <tspan x='400' dy='70'>
+                                    I enjoy learning new software technologies that I have not previous learn.
+                                </tspan>
+                                <tspan x='400' dy='70'>
+                                    The following are some of my software skill sets
+                                </tspan>
+                    </text>
+                </g>
+
+                <g className='about_table1'>
+                    <text   x='400px' y='700px'
+                            textAnchor='left'
+                            fontSize='50px'
+                            fill='hsl(0,0%,50%)'>
+                                <tspan x='400' dy='10'>Javascript</tspan>
+                                <tspan x='400' dy='70'>Python</tspan>
+                                <tspan x='400' dy='70'>C/C++</tspan>
+                                <tspan x='400' dy='70'>C#</tspan>
+                                <tspan x='400' dy='70'>Java</tspan>
+                                <tspan x='400' dy='70'>SQL</tspan>
+                    </text>
+                </g>
+
+                <g className='about_table2'>
+                    <text   x='400px' y='1400px'
+                            textAnchor='left'
+                            fontSize='50px'
+                            fill='hsl(0,0%,50%)'>
+                                <tspan x='400' dy='10'>Nodejs</tspan>
+                                <tspan x='400' dy='70'>React</tspan>
+                                <tspan x='400' dy='70'>Expressjs</tspan>
+                                <tspan x='400' dy='70'>MySql</tspan>
+                                <tspan x='400' dy='70'>Unity Engine</tspan>
+                    </text>
+                </g>
+
+                <path   className="textline1"
+                            d='M 320 200 v 1600' 
+                            stroke='hsl(170,100%,30%)'
+                            strokeWidth='10px'/>
+
+            </svg>
 
         </div>
     );
